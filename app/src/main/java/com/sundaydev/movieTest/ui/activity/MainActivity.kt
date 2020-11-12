64a993b902d358b71d04347ae60d0f3a71b4ab77
kimdongsik <dongsik.kim@navercorp.com>
2020-11-12 17:04:15 +0900

    메인액티비티 코드 정리

--- app/src/main/java/com/sundaydev/movieTest/ui/activity/MainActivity.kt
+++ app/src/main/java/com/sundaydev/movieTest/ui/activity/MainActivity.kt
@@ -20,41 +20,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
-        setSupportActionBar(binding.toolbar)
-        appBarConfiguration = AppBarConfiguration(navIds)
-        initNavigation()
+        binding.apply {
+            lifecycleOwner = this@MainActivity
+            setSupportActionBar(toolbar)
+            appBarConfiguration = AppBarConfiguration(navIds)
+            initNavigation()
+        }
     }
 
-    override fun onBackPressed() {
-        when (findNavController(R.id.nav_host).currentDestination?.id) {
-            R.id.movieFragment, R.id.tvFragment, R.id.peopleFragment -> finish()
-            else -> super.onBackPressed()
-        }
+    override fun onBackPressed() = when (findNavController(R.id.nav_host).currentDestination?.id) {
+        R.id.movieFragment, R.id.tvFragment, R.id.peopleFragment -> finish()
+        else -> super.onBackPressed()
     }
 
     override fun onSupportNavigateUp(): Boolean = findNavController(R.id.nav_host).navigateUp(appBarConfiguration)
 
-    private fun initNavigation() {
-        findNavController(R.id.nav_host).run {
-            setupActionBarWithNavController(this, appBarConfiguration)
-            bottom_nav.setupWithNavController(this)
-            addOnDestinationChangedListener { controller, destination, arguments ->
-                when (destination.id) {
-                    R.id.splashFragment -> {
-                        toolbar.visibility = View.GONE
-                        bottom_nav.visibility = View.GONE
-                    }
-                    R.id.detailFragment, R.id.peopleDetailFragment -> {
-                        bottom_nav.visibility = View.GONE
-                    }
-                    else -> {
-                        toolbar.visibility = View.VISIBLE
-                        bottom_nav.visibility = View.VISIBLE
-                    }
-                }
-            }
-        }
+    private fun initNavigation() = findNavController(R.id.nav_host).run {
+        setupActionBarWithNavController(this, appBarConfiguration)
+        bottom_nav.setupWithNavController(this)
+        addOnDestinationChangedListener { _, destination, _ -> updateViewByDestination(destination.id) }
     }
 
+    private fun updateViewByDestination(destinationId: Int) {
+        when (destinationId) {
+            R.id.splashFragment -> {
+                toolbar.visibility = View.GONE
+                bottom_nav.visibility = View.GONE
+            }
+            R.id.detailFragment, R.id.peopleDetailFragment -> {
+                bottom_nav.visibility = View.GONE
+            }
+            else -> {
+                toolbar.visibility = View.VISIBLE
+                bottom_nav.visibility = View.VISIBLE
+            }
+        }
+
+    }
 }
