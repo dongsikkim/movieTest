b1303b4d19192dd80654335d05ba620ae1b35dfe
kimdongsik <dongsik.kim@navercorp.com>
2020-11-12 14:41:21 +0900

    미사용 로직 제거

--- app/src/main/java/com/sundaydev/movieTest/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/sundaydev/movieTest/viewmodel/BaseViewModel.kt
@@ -1,23 +1,14 @@
 package com.sundaydev.movieTest.viewmodel
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.SerialDisposable
 import org.koin.core.KoinComponent
 
-enum class ViewStatus {
-    INIT, LOADING, DONE, FAIL
-}
-
 open class BaseViewModel : ViewModel(), KoinComponent {
-    val viewStatus = MutableLiveData(ViewStatus.INIT)
     val disposable: CompositeDisposable = CompositeDisposable()
-    val serial: SerialDisposable = SerialDisposable()
 
     override fun onCleared() {
         disposable.dispose()
-        serial.dispose()
         super.onCleared()
     }
 }
