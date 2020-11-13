12d53db0d88f810013a35c84ff508abfc521e97d
kimdongsik <dongsik.kim@navercorp.com>
2020-11-13 18:37:44 +0900

    코인 버전 업데이트(실행 불가)

--- app/src/main/java/com/sundaydev/movieTest/MovieApplication.kt
+++ app/src/main/java/com/sundaydev/movieTest/MovieApplication.kt
@@ -9,13 +9,14 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MovieApplication : Application(), KoinComponent {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@MovieApplication)
-            androidLogger()
+            androidLogger(Level.NONE)
             modules(applicationModules)
         }
         setDarkTheme()
